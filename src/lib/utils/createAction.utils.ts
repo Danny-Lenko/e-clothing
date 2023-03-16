b901fc90c82a0b4403d0a53cab56f2684f184fc7
Danny-Lenko <devdanny.14@gmail.com>
2023-03-16 08:34:47 +0200

    to createAction.utils: added matchable type for with matcher function, added the function and its overloadings

--- src/lib/utils/createAction.utils.ts
+++ src/lib/utils/createAction.utils.ts
@@ -1,5 +1,28 @@
 import { AnyAction } from 'redux'
 
+type Matchable<AC extends () => AnyAction> = AC & {
+   type: ReturnType<AC>['type']
+   match(action: AnyAction): action is ReturnType<AC>
+}
+
+export function withMatcher<AC extends () => AnyAction & { type: string }>(
+   actionCreator: AC
+): Matchable<AC>
+
+export function withMatcher<
+   AC extends (...args: any[]) => AnyAction & { type: string }
+>(actionCreator: AC): Matchable<AC>
+
+export function withMatcher(actionCreator: Function) {
+   const type = actionCreator().type
+   return Object.assign(actionCreator, {
+      type,
+      match(action: AnyAction) {
+         return action.type === type
+      },
+   })
+}
+
 export type ActionWithPayload<T, P> = {
    type: T
    payload: P
@@ -14,7 +37,10 @@ export function createAction<T extends string, P>(
    payload: P
 ): ActionWithPayload<T, P>
 
-export function createAction<T extends string>(type: T, payload: void): Action<T>
+export function createAction<T extends string>(
+   type: T,
+   payload: void
+): Action<T>
 
 export function createAction<T extends string, P>(type: T, payload: P) {
    return { type, payload }
