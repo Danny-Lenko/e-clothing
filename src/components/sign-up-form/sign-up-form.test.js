a6b35fda8b7fd042a37a4312b5a4666a5ab45804
Valerii Danylenko <devdanny.14@gmail.com>
2023-05-17 08:51:31 +0300

    refactored sign-up-form.test

--- src/components/sign-up-form/sign-up-form.test.js
+++ src/components/sign-up-form/sign-up-form.test.js
@@ -1,28 +1,15 @@
-import { render, fireEvent, waitFor, screen } from '@testing-library/react'
-import { Provider } from 'react-redux'
-import configureStore from 'redux-mock-store'
+import { render, screen } from '../../test-utils'
+import userEvent from '@testing-library/user-event'
 import SignUpForm from './sign-up-form.component'
 import { signUpStart } from '../../lib/store/user/user.action'
+import { store } from '../../lib/store/store'
 
-const mockStore = configureStore([])
+store.dispatch = jest.fn()
 
 describe('SignInForm component', () => {
-   let store
-
-   beforeEach(() => {
-      store = mockStore({
-         user: {},
-      })
-      store.dispatch = jest.fn()
-      // eslint-disable-next-line testing-library/no-render-in-setup
-      render(
-         <Provider store={store}>
-            <SignUpForm />
-         </Provider>
-      )
-   })
-
    test('renders the correct text', () => {
+      render(<SignUpForm />)
+
       expect(
          screen.getByText(/Sign up with your email & password/i)
       ).toBeInTheDocument()
@@ -32,49 +19,32 @@ describe('SignInForm component', () => {
       expect(screen.getByLabelText(/Confirm Password/i)).toBeInTheDocument()
    })
 
-   test('handles field values change', async () => {
-      fireEvent.change(screen.getByLabelText(/Name/i), {
-         target: { value: 'testname' },
-      })
-      fireEvent.change(screen.getByLabelText('User Email'), {
-         target: { value: 'test@test.com' },
-      })
-      fireEvent.change(screen.getByLabelText('Password'), {
-         target: { value: 'testpassword' },
-      })
-      fireEvent.change(screen.getByLabelText(/Confirm Passwor/i), {
-         target: { value: 'testconfirm' },
-      })
-      await waitFor(() =>
-         expect(screen.getByLabelText(/Name/i)).toHaveValue('testname')
-      )
-      await waitFor(() =>
-         expect(screen.getByLabelText(/Email/i)).toHaveValue('test@test.com')
-      )
-      await waitFor(() =>
-         expect(screen.getByLabelText('Password')).toHaveValue('testpassword')
-      )
-      await waitFor(() =>
-         expect(screen.getByLabelText(/Confirm Password/i)).toHaveValue(
-            'testconfirm'
-         )
+   test('handles field values change', () => {
+      render(<SignUpForm />)
+
+      userEvent.type(screen.getByLabelText(/Name/i), 'testname')
+      expect(screen.getByLabelText(/Name/i)).toHaveValue('testname')
+
+      userEvent.type(screen.getByLabelText('User Email'), 'test@test.com')
+      expect(screen.getByLabelText(/Email/i)).toHaveValue('test@test.com')
+
+      userEvent.type(screen.getByLabelText('Password'), 'testpassword')
+      expect(screen.getByLabelText('Password')).toHaveValue('testpassword')
+
+      userEvent.type(screen.getByLabelText(/Confirm Passwor/i), 'testconfirm')
+      expect(screen.getByLabelText(/Confirm Password/i)).toHaveValue(
+         'testconfirm'
       )
    })
 
    test('dispatches emailSignUpStart action on form submit', async () => {
-      fireEvent.change(screen.getByLabelText(/User Name/i), {
-         target: { value: 'test' },
-      })
-      fireEvent.change(screen.getByLabelText(/User Email/i), {
-         target: { value: 'test@example.com' },
-      })
-      fireEvent.change(screen.getByLabelText('Password'), {
-         target: { value: 'password' },
-      })
-      fireEvent.change(screen.getByLabelText(/Confirm Password/i), {
-         target: { value: 'password' },
-      })
-      fireEvent.click(screen.getByRole('button', /Sign Up/i))
+      render(<SignUpForm />)
+
+      userEvent.type(screen.getByLabelText(/Name/i), 'test')
+      userEvent.type(screen.getByLabelText(/User Email/i), 'test@example.com')
+      userEvent.type(screen.getByLabelText('Password'), 'password')
+      userEvent.type(screen.getByLabelText(/Confirm Password/i), 'password')
+      userEvent.click(screen.getByRole('button', /Sign Up/i))
       expect(store.dispatch).toHaveBeenCalledWith(
          signUpStart('test@example.com', 'password', 'test')
       )
