8c4c46862b9ec11c28bb6f6032375f3124d7e7ee
Valerii Danylenko <devdanny.14@gmail.com>
2023-05-24 10:14:07 +0300

    have written the basic cypress test

--- cypress/e2e/user.cy.ts
+++ cypress/e2e/user.cy.ts
@@ -1,5 +1,12 @@
-describe('template spec', () => {
+describe('visit homepage', () => {
    it('passes', () => {
-     cy.visit('http://localhost:3000')
+      cy.visit('http://localhost:3000')
+      cy.findByRole('link', { name: /shop/i })
+         .should('have.css', 'cursor')
+         .and('match', /pointer/)
+      cy.findByRole('link', { name: /shop/i }).click()
+      cy.url().should('include', '/shop')
+      cy.findByRole('link', {  name: /group/i}).click()
+      cy.url().should('include', '/')
    })
- })
+})
